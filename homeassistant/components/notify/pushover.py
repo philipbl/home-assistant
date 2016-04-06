7f2ce95171cbb59db8e883e9e4fc9c1661e00c0d
Philip Lundrigan <philiplundrigan@gmail.com>
2016-04-06 12:47:26 -0600

    Add device option to Pushover
    device can now be specified either when setting up the notification or
    when sending the message.

--- homeassistant/components/notify/pushover.py
+++ homeassistant/components/notify/pushover.py
@@ -14,20 +14,24 @@ from homeassistant.helpers import validate_config
 REQUIREMENTS = ['python-pushover==0.2']
 _LOGGER = logging.getLogger(__name__)
 
+CONF_USER_KEY = 'user_key'
+CONF_DEVICE = 'device'
+
 
 # pylint: disable=unused-variable
 def get_service(hass, config):
     """Get the Pushover notification service."""
     if not validate_config({DOMAIN: config},
-                           {DOMAIN: ['user_key', CONF_API_KEY]},
+                           {DOMAIN: [CONF_USER_KEY, CONF_API_KEY]},
                            _LOGGER):
         return None
 
     from pushover import InitError
 
     try:
-        return PushoverNotificationService(config['user_key'],
-                                           config[CONF_API_KEY])
+        return PushoverNotificationService(config[CONF_USER_KEY],
+                                           config[CONF_API_KEY],
+                                           config.get(CONF_DEVICE))
     except InitError:
         _LOGGER.error(
             "Wrong API key supplied. "
@@ -39,19 +43,20 @@ def get_service(hass, config):
 class PushoverNotificationService(BaseNotificationService):
     """Implement the notification service for Pushover."""
 
-    def __init__(self, user_key, api_token):
+    def __init__(self, user_key, api_token, device):
         """Initialize the service."""
         from pushover import Client
         self._user_key = user_key
         self._api_token = api_token
         self.pushover = Client(
-            self._user_key, api_token=self._api_token)
+            self._user_key, api_token=self._api_token, device=device)
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
         from pushover import RequestError
 
         try:
-            self.pushover.send_message(message, title=kwargs.get(ATTR_TITLE))
+            self.pushover.send_message(message, title=kwargs.get(ATTR_TITLE),
+                                       device=kwargs.get(CONF_DEVICE))
         except RequestError:
             _LOGGER.exception("Could not send pushover notification")
